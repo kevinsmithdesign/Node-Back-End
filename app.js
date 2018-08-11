be1e32e204a50cda6a8ea98bc42038db679cdedf
Kevin Smith <mrkevinsmithdesign@yahoo.com>
2018-08-11 13:40:57 -0500

    Added Body Parsing and CORS

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 const app = express();
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', '*');
+    if(req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT','POST','PATCH','DELETE','GET');
+        return res.status(200).json({});
+    }
+    next();
+});
 
 // Routes that handle requests
 app.use('/products', productRoutes);
